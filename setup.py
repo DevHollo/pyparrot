d962350e2041281455e938db52e355fb32b9f01e
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-16 22:38:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyParrotSay',
-    version='0.0.4',
+    version='0.0.5',
     scripts=['pyparrotsay.py'],
     author="Hollo",
     author_email="colin.boyett10@gmail.com",
