f2dca5108dc0539fa5bcaebbeea22f63f287a521
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-16 22:50:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as mdfile:
 
 setup(
     name='PyParrotSay',
-    version='0.0.6',
+    version='0.0.7',
     scripts=['pyparrotsay.py'],
     author="Hollo",
     author_email="colin.boyett10@gmail.com",
