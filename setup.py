cf7cd1de9a6696d3b063caacf88c1ec26e7d9f53
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-16 22:08:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author="Hollo",
     author_email="colin.boyett10@gmail.com",
     long_description="# PyParrot\nMake a funny parrot say anything!\nUsage:\n```py\nfrom pyparrot import Parrotsay\nparrot = ParrotSay()\n\nparrot.say(\"Hello!\")\n```",
+    long_description_content_type="text/markdown",
     requires=[
         "colorama"
     ],
