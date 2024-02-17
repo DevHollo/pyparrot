8e126af4cca3cda9cd522694e4bfb8565eb3fa27
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-16 22:37:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     scripts=['pyparrotsay.py'],
     author="Hollo",
     author_email="colin.boyett10@gmail.com",
-    long_description="# PyParrotSay\nMake a funny parrot say anything!\nUsage:\n```py\nfrom pyparrotsay import ParrotSay\n\nparrot = ParrotSay()\n\nparrot.say(\"Hello!\")\n```",
+    long_description="# PyParrotSay\nMake a funny parrot say anything!<br/>\nUsage:\n```py\nfrom pyparrotsay import ParrotSay\n\nparrot = ParrotSay()\n\nparrot.say(\"Hello!\")\n```",
     long_description_content_type="text/markdown",
     install_requires=[
         "colorama==0.4.6"
