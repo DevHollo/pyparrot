411486ec39bcfa61dcf0c9a5b43e378996b60280
Hollo <127456155+DevHollo@users.noreply.github.com>
2024-02-16 22:06:49 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='PyParrot',
+    version='0.0.1',
+    scripts=['pyparrot.py'],
+    author="Hollo",
+    author_email="colin.boyett10@gmail.com",
+    long_description="# PyParrot\nMake a funny parrot say anything!\nUsage:\n```py\nfrom pyparrot import Parrotsay\nparrot = ParrotSay()\n\nparrot.say(\"Hello!\")\n```",
+    requires=[
+        "colorama"
+    ],
+    url="https://github.com/DevHollo/pyparrot"
+)
